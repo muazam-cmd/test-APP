d1b7417ff249c61f9326f9d23b1812d2403c643c
muazam-cmd <muazamshahid7@gmail.com>
2023-01-03 13:36:11 +0400

    changed div content

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>hello!</div>
+        <div>hey HI!</div>
         <div>testing Commits</div>
       </header>
     </div>
